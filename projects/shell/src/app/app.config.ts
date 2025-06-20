905aaff8681b0890ac94c2e1805e866b19e303b4
Roberto Orona <jrov.90@gmail.com>
2025-06-20 15:19:43 -0600

    version funcional con msal

--- projects/shell/src/app/app.config.ts
+++ projects/shell/src/app/app.config.ts
@@ -3,10 +3,34 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAuth } from '../../../../libs/auth/auth.provider';
+import {   withComponentInputBinding } from '@angular/router';
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { MsalGuard, MsalInterceptor, MSAL_INSTANCE, MSAL_GUARD_CONFIG, MsalService, MsalBroadcastService } from '@azure/msal-angular';
+import { MSALInstanceFactory, MSALGuardConfigFactory } from '../../../../libs/auth/msal-instance.factory' // './core/services/auth/msal-instance.factory';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes),
+     provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(),
+    provideAnimations(),
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory
+    },
+    {
+      provide: MSAL_GUARD_CONFIG,
+      useFactory: MSALGuardConfigFactory
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true
+    },
+    MsalService,
+    MsalGuard,
+    MsalBroadcastService
     //...provideAuth() // reutilizas desde lib
   ]
 };
--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { loadRemoteModule } from '@angular-architects/module-federation';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { MsalGuard } from '@azure/msal-angular';
 
 export const routes: Routes = [
     {
@@ -16,7 +17,8 @@ export const routes: Routes = [
                     //remoteEntry: 'https://swpyme.z19.web.core.windows.net/remoteEntry.js',
                     exposedModule: './routes'
                 })
-                .then(m => m.MFE_USERS_ROUTES)
+                .then(m => m.MFE_USERS_ROUTES),
+                 canActivate: [MsalGuard]
         },
         ]
     },
