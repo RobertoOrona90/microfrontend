c01997f15d712d97f03aca1d0d3a26e680a271d9
Roberto Orona <jrov.90@gmail.com>
2025-06-23 07:19:11 -0600

    login to home

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -7,6 +7,7 @@ export const routes: Routes = [
     {
         path: '',
         component: MainLayoutComponent,
+         canActivate: [MsalGuard],
         children: [
         {
             path: 'users',
